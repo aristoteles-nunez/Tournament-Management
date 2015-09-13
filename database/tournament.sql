92053c6efe3fb1c3f6bafe4a97e35eb085b4e08f
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2015-09-12 20:37:37 -0500

    Tables events and matches

--- database/tournament.sql
+++ database/tournament.sql
@@ -1,8 +1,23 @@
 -- Table definitions for the tournament project.
 --
 
-CREATE TABLE players (firstname TEXT,
-					  lastname TEXT,
-                      id SERIAL );
+CREATE TABLE players (
+	firstname TEXT,
+	lastname TEXT,
+	id SERIAL 
+);
 
+CREATE TABLE events (
+	name TEXT,
+	event_date TIMESTAMP,
+	id SERIAL
+);
 
+CREATE TABLE matches(
+	player_one_id INTEGER,
+	player_two_id INTEGER,
+	event_id INTEGER,
+	player_one_score DOUBLE PRECISION,
+	player_two_score DOUBLE PRECISION,
+	round_number INTEGER
+);
