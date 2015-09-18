6ad16954d0aafece03b6ad65540f0e380c7f65ff
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2015-09-18 11:55:42 -0500

    feat: Add Procedures
    Add procedures activePlayersInEvent, matchesByPlayersInEvent, standings

--- database/tournament.sql
+++ database/tournament.sql
@@ -25,5 +25,53 @@ CREATE TABLE matches (
 	player_one_score DOUBLE PRECISION,
 	player_two_score DOUBLE PRECISION,
 	event INTEGER REFERENCES events(id),
-	round_number INTEGER
+	round_number INTEGER,
+	id SERIAL PRIMARY KEY
 );
+
+CREATE OR REPLACE FUNCTION activePlayersInEvent(actualEvent INTEGER)
+RETURNS TABLE(
+	id INTEGER,
+	name TEXT
+) AS $func$
+SELECT players.id, (firstname || ' ' || lastname) as name 
+        FROM players, playersInEvent WHERE event=actualEvent 
+        AND players.id=playersInEvent.player;
+$func$  LANGUAGE sql;
+
+
+CREATE OR REPLACE FUNCTION matchesByPlayersInEvent(actualEvent INTEGER)
+RETURNS TABLE(
+	id INTEGER,
+	name TEXT,
+	score DOUBLE PRECISION,
+	match INTEGER
+) AS $func$
+SELECT activePlayers.id, activePlayers.name, 
+	CASE WHEN activePlayers.id = matches.player_one AND matches.player_one_score != null
+			THEN  matches.player_one_score
+        WHEN activePlayers.id = matches.player_two AND matches.player_two_score != null
+        THEN matches.player_two_score
+        ELSE 0.0
+    END as score
+	, matches.id as match 
+	FROM (SELECT * FROM activePlayersInEvent(actualEvent)) as activePlayers
+	LEFT JOIN matches ON (activePlayers.id = matches.player_one) or 
+        (activePlayers.id = matches.player_two)
+$func$  LANGUAGE sql;
+
+
+CREATE OR REPLACE FUNCTION standings(actualEvent INTEGER)
+RETURNS TABLE(
+	id INTEGER,
+	name TEXT,
+	points DOUBLE PRECISION,
+	matches BIGINT 
+) AS $func$
+SELECT id, name, SUM(matchesByPlayers.score) AS points,
+		COUNT(matchesByPlayers.match) AS matches
+        FROM matchesByPlayersInEvent(actualEvent) as matchesByPlayers
+        GROUP BY id, name 
+        ORDER BY points DESC, name ASC;
+$func$  LANGUAGE sql;
+        
